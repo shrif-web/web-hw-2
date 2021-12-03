461cdfc5c7916f3062b043bbd4f01d9c0d6c076d
amm266 <a.m.mohammadi266@gmail.com>
2021-12-03 20:25:37 +0330

    update css

--- Front/js.js
+++ Front/js.js
@@ -2,6 +2,30 @@ function change_gradiant() {
     r1 = Math.floor(Math.random() * 255)
     g1 = Math.floor(Math.random() * 255)
     b1 = Math.floor(Math.random() * 255)
-    document.getElementById("wave_image").style = `background-image:linear-gradient(to bottom, rgba(${r1}, ${g1}, ${b1}, 0.52), rgba(${255-r1}, ${255-g1}, ${255-b1}, 0.73)), url(\"files/ITOpsKanban.jpg\");`
-
+    document.getElementById("wave_image").style = `background-image:linear-gradient(to right, rgba(${r1}, ${g1}, ${b1}, 0.52), rgba(${255 - r1}, ${255 - g1}, ${255 - b1}, 0.73)), url(\"../files/ITOpsKanban.jpg\");`
 }
+
+function toast() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.show())
+    console.log("on click" + toastList)
+}
+
+function small_gallery() {
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (width <= 600) {
+        document.getElementById("Gallery_600").style.display = "block";
+        document.getElementById("Gallery").style.display = "none";
+    }
+    else {
+        document.getElementById("Gallery_600").style.display = "none";
+        document.getElementById("Gallery").style.display = "block";
+
+    }
+}
+
+small_gallery()
+window.onresize = small_gallery
